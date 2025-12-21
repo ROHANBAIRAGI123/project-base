a15753706bcd2828905e5ca6b3c200e2d4b5016b
Rohan Bairagi <rohanbairagi40@gmail.com>
2025-12-21 21:10:00 +0530

    connect DB and healthcheck router create

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDB from './db/index.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+connectDB().then(() => {
+    console.log('Database connected successfully');
+    app.listen(PORT, () => {
     console.log(`Server is running on link http://localhost:${PORT}`);
+});
+}).catch((error) => {
+    console.error('Database connection failed:', error);
+    process.exit(1);
 });
